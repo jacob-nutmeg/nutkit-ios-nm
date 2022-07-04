080bccc10d71e9591f081adf311ca9f9db33ab19
Jacob Whitehead <jacob.whitehead@nutmeg.com>
2022-07-04 13:26:40 +0100

    0.0.4

--- module-update.sh
+++ module-update.sh
@@ -2,10 +2,11 @@
 echo "Copying files"
 cp -r ./../packages/nk-tokens/dist/default/ios/. Sources/NutKit/
 echo "Completed copy"
-VERSION="0.0.3"
+VERSION="0.0.4"
 #$(jq .version ./../packages/nk-tokens/package.json)
 echo "Commiting and setting version"
-git commit -a -m $VERSION
+git add -A
+git commit -m $VERSION
 git tag $VERSION
 git push origin $VERSION
 
